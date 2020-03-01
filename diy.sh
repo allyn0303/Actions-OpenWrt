86c2ffe751a0ff6d663f2520ba195d6c754436e7
allyn0303 <50942687+allyn0303@users.noreply.github.com>
2020-03-01 20:43:51 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,9 +2,9 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#=================================================
+＃作者：P3TERX
+＃博客：https：//p3terx.com
+＃=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
+sed  -i  's / 192.168.1.1 / 192.168.1.123 / g'包/基本文件/文件/ bin / config_generate
